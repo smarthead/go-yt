e9b5bf9c5bde0f474be9774ac92872401f02b8bd
Dmitry Dmitriev <qdimka1991@mail.ru>
2020-10-18 09:38:47 +0300

    Add entities services

---
+++ issues/Issues.go
@@ -0,0 +1,56 @@
+package issues
+
+import (
+	"go-yt/rest"
+	"go-yt/utils"
+)
+
+const (
+	DefaultFields = "id,idReadable,summary,project(id,shortName,name),customFields(name,$type,value(name,login))"
+)
+
+type CustomFieldValue struct {
+	Name  string
+	Login string
+}
+
+type CustomField struct {
+	Name  string      `json:"name"`
+	Type  string      `json:"$type"`
+	Value interface{} `json:"value"`
+}
+
+type Issue struct {
+	Summary      string        `json:"summary"`
+	Description  string        `json:"description"`
+	Project      interface{}   `json:"project"`
+	CustomFields []CustomField `json:"customFields"`
+}
+
+type Service struct {
+	client *rest.Client
+}
+
+func NewIssuesService(client *rest.Client) *Service {
+	return &Service{client: client}
+}
+
+func (s *Service) GetIssues(query string, fields ...string) (*[]Issue, error) {
+	issues := new([]Issue)
+	err := s.client.Get("api/issues", utils.ConstructQuery(query, fields), nil, issues)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, nil
+}
+
+func (s *Service) CreateIssue(issue Issue) error {
+	err := s.client.Post("api/issues", issue, nil, nil)
+	return err
+}
+
+func (s *Service) Comment(i int) error {
+	panic("implement me")
+}
---
+++ projects/projects.go
@@ -0,0 +1,49 @@
+package projects
+
+import (
+	"go-yt/rest"
+	"go-yt/utils"
+)
+
+const (
+	DefaultFields = "id,shortName,name"
+)
+
+type Project struct {
+	Id          string `json:"id"`
+	ShortName   string `json:"shortName"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type Service struct {
+	client *rest.Client
+}
+
+func NewProjectsService(client *rest.Client) *Service {
+	return &Service{client: client}
+}
+
+func (s *Service) GetProjects(query string, fields ...string) (*[]Project, error) {
+	projects := new([]Project)
+
+	err := s.client.Get("api/admin/projects", utils.ConstructQuery(query, fields), nil, projects)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return projects, nil
+}
+
+func (s *Service) GetProjectById(projectId string, fields ...string) (*Project, error) {
+	project := new(Project)
+
+	err := s.client.Get("api/admin/projects/"+projectId, utils.ConstructQuery("", fields), nil, project)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return project, nil
+}
